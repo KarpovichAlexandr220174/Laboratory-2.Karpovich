dfd2f0675027c3b554db0415871f510018901020
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-11 21:06:46 +0600

    Commit

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,6 +1,28 @@
-public class MyArrayList <T> implements MyList{
+public class MyArrayList<T> implements MyList {
     private T[] arr;
     private int size;
+
+    MyArrayList() {
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    public void increaseBuffer() {
+        T[] newArr = (T[]) new Object[arr.length * 2];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
+
+    @Override
+    public void add(Object item) {
+        if (size == arr.length) {
+            increaseBuffer();
+        }
+    }
+
+
     @Override
     public int size() {
         return 0;
@@ -9,11 +31,6 @@ public class MyArrayList <T> implements MyList{
     @Override
     public boolean contains(Object o) {
         return false;
-    }
-
-    @Override
-    public void add(Object item) {
-
     }
 
     @Override
--- src/MyList.java
+++ src/MyList.java
@@ -2,7 +2,7 @@ public interface MyList<T> {
     int size();
 
     boolean contains(Object o);
-
+    void increaseBuffer();
     void add(T item);
 
     void add(T item, int index);
