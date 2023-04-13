30678b84dfec6c0db9be1d0ca563c6db794be367
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-13 15:20:09 +0600

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         MyArrayList<Integer> marr = new MyArrayList<>();
-        //int [] arr = new int[5];
-        marr.add(6);
-        marr.add(7);
-        marr.add(8);
-        marr.add(9);
-        marr.add(10);
-        marr.add(11);
-        marr.add(12);
+//        marr.add(6);
+//        marr.add(7);
+//        marr.add(8);
+//        marr.add(9);
+//        marr.add(10);
+//        marr.add(11);
+//        marr.add(12);
+//        System.out.println(marr.indexOf(11));
         //
 //        marr.add(3, 0);
 //        marr.add(1231, 4);
@@ -24,13 +24,22 @@ public class Main {
 //        System.out.println(marr1.get(0));
 //        System.out.println(marr1.contains("Sasha"));
         //System.out.println(marr.contains(100));
-        marr.clear();
-        System.out.println(marr.size());
-        MyArrayList<String> src = new MyArrayList<>();
-        src.add("Sasha", 0);
-        System.out.println(src.get(0));
-        System.out.println(src.contains("Sasha"));
-        src.remove("Sasha");
-        System.out.println(src.contains("Sasha"));
+//        marr.clear();
+//        System.out.println(marr.size());
+//        MyArrayList<String> src = new MyArrayList<>();
+//        src.add("Sasha", 0);
+//        System.out.println(src.get(0));
+//        System.out.println(src.contains("Sasha"));
+//        //src.remove("Sasha");
+//        System.out.println(src.contains("Sasha"));
+//        System.out.println(src.indexOf("Sasha"));
+          MyArrayList<String> src1 = new MyArrayList<>();
+          src1.add("Sasha", 0);
+          src1.add("Sasha1", 1);
+          src1.add("Sasha2", 2);
+          src1.add("Sasha3", 3);
+        System.out.println(src1.size());
+        System.out.println(src1.indexOf("Sasha1"));
+        System.out.println(src1.indexOf("Sasha3"));
     }
 }
