9659f746eedf1851c59c492ca97f7eed91262297
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-27 19:52:57 +0600

    Commit

---
+++ src/MyLinkedListStack.java
@@ -0,0 +1,30 @@
+
+
+
+public class MyLinkedListStack<T> implements ListForStack<T> {
+
+    @Override
+    public void push(T element) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
