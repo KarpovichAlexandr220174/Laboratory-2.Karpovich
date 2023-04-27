a2ad974c1bd3a0f3821ae4b4a840a64f2a96d872
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-27 19:56:54 +0600

    Commit

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -3,6 +3,12 @@ import java.util.EmptyStackException;
 
 public class MyLinkedListStack<T> implements ListForStack<T> {
 
+    private LinkedList<T> linkedList;
+
+    MyLinkedListStack() {
+        linkedList = new LinkedList<>();
+    }
+
     @Override
     public void push(T element) {
 
