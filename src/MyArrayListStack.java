cad1821f186536c8360b238af4a6d3614a14b0cd
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-04-27 15:30:05 +0600

    Commit

---
+++ src/MyArrayListStack.java
@@ -0,0 +1,28 @@
+public class MyArrayListStack<T> implements ListForStack<T> {
+
+
+    @Override
+    public void push(T element) {
+
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
