b524b65c9e5a648f9ae90189436b2ec0b6e857c9
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-05-01 19:48:05 +0600

    final1commit

--- src/ArrayListStack.java
+++ src/ArrayListStack.java
@@ -1,46 +1,60 @@
+// Importing the necessary utilities
+
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.Iterator;
 
+// Implementing ListForStack interface and Iterable for ArrayListStack
 public class ArrayListStack<T> implements ListForStack<T>, Iterable<T> {
-
+    // Declare an ArrayList to store elements
     private ArrayList<T> arraylist;
 
+    // Constructor to initialize the ArrayList
     ArrayListStack() {
         arraylist = new ArrayList<>();
     }
 
+    // Method to add element to the stack
     @Override
     public void push(T element) {
         arraylist.add(element);
     }
 
+    // Method to remove and return the top element from the stack
     @Override
     public T pop() {
+        // Check if the stack is empty
         if (isEmpty()) {
             throw new EmptyStackException();
         }
+        // Remove and return the top element from the stack
         return arraylist.remove(size() - 1);
     }
 
+    // Method to return the top element of the stack without removing it
     @Override
     public T peek() {
+        // Check if the stack is empty
         if (isEmpty()) {
             throw new EmptyStackException();
         }
+        // Return the top element of the stack
         return arraylist.get(size() - 1);
     }
 
+    // Method to check if the stack is empty
     @Override
     public boolean isEmpty() {
         return arraylist.isEmpty();
     }
 
+    // Method to return the size of the stack
     @Override
     public int size() {
         return arraylist.size();
     }
 
+    // Method to return an iterator for the stack
     @Override
     public Iterator<T> iterator() {
         return arraylist.iterator();
--- src/LinkedListStack.java
+++ src/LinkedListStack.java
@@ -1,48 +1,59 @@
+// Importing necessary Java libraries
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.EmptyStackException;
 
+// Class definition with type parameter T
 public class LinkedListStack<T> implements ListForStack<T>, Iterable<T> {
 
+    // Declare linked list of type T
     private LinkedList<T> linkedList;
 
+    // Constructor for creating an empty linked list
     LinkedListStack() {
         linkedList = new LinkedList<>();
     }
 
+    // Method to add an element to the top of the stack
     @Override
     public void push(T element) {
-        linkedList.addFirst(element);
+        linkedList.addFirst(element); // Adds the specified element at the beginning of the linked list
     }
 
+    // Method to remove and return the top element from the stack
     @Override
     public T pop() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
+        if (isEmpty()) { // If stack is empty
+            throw new EmptyStackException(); // Throw EmptyStackException
         }
-        return linkedList.removeFirst();
+        return linkedList.removeFirst(); // Removes and returns the first element in the linked list
     }
 
+    // Method to return the top element of the stack without removing it
     @Override
     public T peek() {
-        if (isEmpty()) {
-            throw new EmptyStackException();
+        if (isEmpty()) { // If stack is empty
+            throw new EmptyStackException(); // Throw EmptyStackException
         }
-        return linkedList.getFirst();
+        return linkedList.getFirst(); // Returns the first element in the linked list
     }
 
+    // Method to check if the stack is empty
     @Override
     public boolean isEmpty() {
-        return linkedList.isEmpty();
+        return linkedList.isEmpty(); // Returns true if the stack is empty, false otherwise
     }
 
+    // Method to return the number of elements in the stack
     @Override
     public int size() {
-        return linkedList.size();
+        return linkedList.size(); // Returns the size of the linked list
     }
 
+    // Method to return an iterator over the elements in the stack
     @Override
     public Iterator<T> iterator() {
-        return linkedList.iterator();
+        return linkedList.iterator(); // Returns an iterator over the elements in the linked list
     }
 }
